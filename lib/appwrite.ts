9ca657d0263246595503fefa4bf818d194424b4f
Clebert Nkuranga <nkurclebert@gmail.com>
2025-07-16 18:32:03 +0200

    Initial commit, set up the base file & folder structure for the rest of the app

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+    endpoint: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT ,
+    platform: "com.clebert.FoodOrdering",
+    projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID, 
+}
